d8a39f2d3f147a926ee5240b152d38205f008739
abo969 <zubo.yu9@gmail.com>
2022-06-13 23:06:38 +0800

    98. 验证二叉搜索树

---
+++ tree/98.cpp
@@ -0,0 +1,51 @@
+/**
+ * 98. 验证二叉搜索树
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * 有效 二叉搜索树定义如下：
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool dfs(TreeNode* root, long minx, long maxx) {
+        if (!root) return true;
+
+        // 不在(minx, maxx)范围内，直接返回false
+        if (root->val <= minx || root->val >= maxx) return false;
+
+        if (root->left) {   // 检查左子树
+            if (!dfs(root->left, minx, root->val)) return false;
+        }
+
+        if (root->right) {  // 检查右子树
+            if (!dfs(root->right, root->val, maxx)) return false;
+        }
+
+        return true;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if (!root) return true;
+        if (!root->left && !root->right) return true;
+
+        return dfs(root, LONG_MIN, LONG_MAX);
+    }
+};
